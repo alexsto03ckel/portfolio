37ce314639798231115f5e5dcb240eaddd5bff89
433solutions <166217798+433solutions@users.noreply.github.com>
2024-04-06 20:25:44 +0800

    Update from-handler.php

--- from-handler.php
+++ from-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'alexsto03ckel@gmail.com';
+$email_from = 'info@skillseekho.org';
 
 $email_subject = 'New Form Submission!';
 
